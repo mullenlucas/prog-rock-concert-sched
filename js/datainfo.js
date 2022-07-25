23dfdc75418ae0eecc836a3f489ca37f03b08c7f
mullenslucas <mullenlucas@gmail.com>
2022-07-25 18:15:37 -0300

    add images and data structure in js

---
+++ js/datainfo.js
@@ -0,0 +1,77 @@
+export const schedule = [
+  {
+    id: "sch1",
+    icon: "./imgs/headphone-icon.png",
+    title: "Music",
+    text: ""
+  },
+  {
+    id: "sch2",
+    icon: "./imgs/artist-icon.png",
+    title: "Artists",
+    text: ""
+  },
+  {
+    id: "sch3",
+    icon: "./imgs/camping-icon.png",
+    title: "Camping",
+    text: ""
+  },
+  {
+    id: "sch4",
+    icon: "./imgs/event-icon.png",
+    title: "Events",
+    text: ""
+  },
+  {
+    id: "sch5",
+    icon: "./imgs/merch-icon.png",
+    title: "Merch",
+    text: ""
+  }
+]
+
+export const artists = [
+  {
+    id: "ar1",
+    img: "./imgs/pinkfloyd-img.jpg",
+    bandName: "Pink Floyd",
+    style: "Progressive psychedelic rock",
+    desc: "Fewer bands are as iconic and as legendary as Pink Floyd. From early psychedelic experimentations in the late 60s, to the emblematic Dark Side of the Moon album, across The Wall to more earthly compositions, this band remains to this day one of the most succesful and diverse bands in rock history",
+  },
+  {
+    id: "ar2",
+    img: "./imgs/yes-img.jpg",
+    bandName: "Yes",
+    style: "Progressive Symphonic rock",
+    desc: "A conglomerate of virtuous musicians, this band is known for their sophisticated compositions and masterful excecutions",
+  },
+  {
+    id: "ar3",
+    img: "./imgs/genesis-img.jfif",
+    bandName: "Genesis",
+    style: "Progressive Theatrical rock",
+    desc: "Before Phil Collins made it a pop-ish modern rock band, Genesis has is roots in progressive rock by the leading hand of Peter Gabriel, an extravagant performer who made music with theatrical themes",
+  },
+  {
+    id: "ar4",
+    img: "./imgs/pfm-img.jfif",
+    bandName: "Premiata Forneria Marconi",
+    style: "Progressive Symphonic rock",
+    desc: "Another conglomerate of virtuous and academic musicians. They have a distinct sound from their country of Italy and composed probably the greatest progessive rock band of their country",
+  },
+  {
+    id: "ar5",
+    img: "./imgs/gryphon-img.jfif",
+    bandName: "Gryphon",
+    style: "Progressive Folk rock",
+    desc: "This progressive band distinguishes from the rest as they combine celtic folk music into a progressive rock atmosphere, blending into a unique style rarely heard of medieval tones mixed with modern rock",
+  },
+  {
+    id: "ar6",
+    img: "./imgs/focus-img.jfif",
+    bandName: "Focus",
+    style: "Progressive Symphonic rock",
+    desc: "Yet another band of great musicians, this time from Holland, better known for their songs such as 'Hocus Pocus' or 'Sylvia', they also have a vast repertoire of amazing songs",
+  }
+]
